dde9e682ce02abf30ddddfa63a4918c7ccc3dcd2
Jadon <jadwhite2003@gmail.com>
2026-08-18 20:34:48 +0100

    Added basic file upload and updated gitignore will add supabase next

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import time, secrets
+import time, secrets, os, shutil
 
 app = FastAPI()
 storage: dict[str, dict] = {}
 EXPIRY_SECONDS = 300
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 class PasteRequest(BaseModel):
     content: str
@@ -29,6 +32,35 @@ def get_paste(paste_id: str):
         del storage[paste_id]
         raise HTTPException(status_code=404, detail="Not found/expired")
     return{"content": item["content"]}
+
+@app.post("/upload")
+def upload_file(file: UploadFile = File(...)):
+    file_id = secrets.token_urlsafe(6)
+    saved_path = os.path.join(UPLOAD_DIR, file_id)
+    
+    with open(saved_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+    storage[file_id] = {
+        "type": "file",
+        "path": saved_path,
+        "original_name": file.filename,
+        "expires_at": time.time() + EXPIRY_SECONDS
+    }
+    return {"id":file_id, "url": f"/download/{file_id}"}
+
+@app.get("/download/{file_id}")
+def download_file(file_id: str):
+    from fastapi.responses import FileResponse
+
+    item = storage.get(file_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Not found/expired")
+    if time.time() > item["expires_at"]:
+        os.remove(item["path"])
+        del storage[file_id]
+        raise HTTPException(status_code=404, detail="Not found/expired")
+    return FileResponse(item["path"], filename=item["original_name"])
 
 app.add_middleware(
     CORSMiddleware,
